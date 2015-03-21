85c0aee01d7492f78f34a06377b91cf91b1542fe
Marín Salinas <marinssalinas@gmail.com>
2015-03-21 16:44:22 -0600

    USER MODEL

---
+++ app/src/main/java/project/hackmty/partypig/models/User.java
@@ -0,0 +1,17 @@
+package project.hackmty.partypig.models;
+
+/**
+ * Created by marinsalinas on 3/21/15.
+ */
+public class User {
+    private String userId;
+
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+}
